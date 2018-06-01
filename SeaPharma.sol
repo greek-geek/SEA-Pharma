6c8115ffd89954b723b36092c0b33200063f4576
greek-geek <shraddha.m26@gmail.com>
2018-06-01 15:19:47 -0700

    adding smart contract and UI change

--- SeaPharma.sol
+++ SeaPharma.sol
@@ -1,6 +1,27 @@
 pragma solidity ^0.4.24;
 
-contract Owned {
+contract TransModel {
+    uint public transactionId;
+
+    struct Transaction {
+        // uint id;
+        uint drugId;
+        string drugName;
+        uint buyerId;
+        string buyerName;
+        uint sellerId;
+        string sellerName;
+        uint userType;
+        uint quantity;
+        // string location_;
+        address transactionAddedBy;
+    }
+    
+    mapping (uint => Transaction) transactions;
+    uint[] public transactionsArr;
+}
+
+contract Owned is TransModel {
     address owner;
     
     
@@ -12,37 +33,59 @@ contract Owned {
        require(msg.sender == owner);
        _;
    }
+   
+   modifier canAddTransactions (uint _userType) {
+       require(_userType < 4);
+       _;
+   }
+   
+   modifier onlyManufacturererCanAddNewDrugId(uint _userType, uint _drugId) {
+       
+       bool condition = false;
+       
+       bool drugIdExists = false;
+       for(uint i=0; i<transactionsArr.length; i++) {
+           if(transactions[transactionsArr[i]].drugId == _drugId) {
+               drugIdExists = true;
+           }
+       }
+       
+       if(!drugIdExists){
+            if(_userType == 1) {
+                condition = true;
+            }
+       }
+       
+       require(condition);
+       _;
+       
+   }
+   
+
 }
 
 contract SeaPharma is Owned {
     
-    uint public transactionId;
+    uint public blockNumber = block.number;
+    bytes32 public blockHashNow = block.blockhash(blockNumber);
+    bytes32 public blockHashPrevious = block.blockhash(blockNumber - 1);
+
+    // struct Transaction {
+    //     uint id;
+    //     uint drugId;
+    //     string drugName;
+    //     uint buyerId;
+    //     string buyerName;
+    //     uint sellerId;
+    //     string sellerName;
+    //     string userType;
+    //     uint quantity;
+    //     string location_;
+    //     address transactionAddedBy;
+    // }
     
-    struct Transaction {
-        uint id;
-        uint drugId;
-        string drugName;
-        uint buyerId;
-        string buyerName;
-        uint sellerId;
-        string sellerName;
-        uint quantity;
-        string location_;
-        address transactionAddedBy;
-    }
-    
-    struct Transaction_subset {
-        uint drugId;
-        string drugName;
-        uint buyerId;
-        string buyerName;
-        uint sellerId;
-        string sellerName;
-        
-    }
-    
-    mapping (uint => Transaction) transactions;
-    uint[] public transactionsArr;
+    // mapping (uint => Transaction) transactions;
+    // uint[] public transactionsArr;
 
     event transactionInfo(
         uint id,
@@ -52,46 +95,61 @@ contract SeaPharma is Owned {
         string buyerName,
         uint sellerId,
         string sellerName,
+        uint userType,
         uint quantity,
-        string location_,
+        // string location_,
         address transactionAddedBy
     );
     
     function getTransactionId() public returns (uint) {
         return transactionId++;
     }
+
     
-    function setDrugTransaction(uint _drugId, string _drugName, uint _buyerId, string _buyerName, uint _sellerId, string _sellerName, uint _quantity, string _location, address _address) onlyOwner public{
+    function setDrugTransaction(
+        uint _drugId, 
+        string _drugName, 
+        uint _buyerId, 
+        string _buyerName, 
+        uint _sellerId, 
+        string _sellerName, 
+        uint _userType, 
+        uint _quantity) onlyOwner onlyManufacturererCanAddNewDrugId(_userType, _drugId) canAddTransactions(_userType) public returns(uint){
         var _id = getTransactionId();
-        var transaction = transactions[_id];
+        // var transaction = transactions[_id];
         
-        transaction.id = _id;
-        transaction.drugId = _drugId;
-        transaction.drugName = _drugName;   
-        transaction.buyerId = _buyerId;
-        transaction.buyerName = _buyerName;
-        transaction.sellerId = _sellerId;
-        transaction.sellerName = _sellerName;
-        transaction.quantity = _quantity;
-        transaction.location_ = _location;
-        transaction.transactionAddedBy = _address;
+        // transaction.id = _id;
+        transactions[_id].drugId = _drugId;
+        transactions[_id].drugName = _drugName;   
+        transactions[_id].buyerId = _buyerId;
+        transactions[_id].buyerName = _buyerName;
+        transactions[_id].sellerId = _sellerId;
+        transactions[_id].sellerName = _sellerName;
+        transactions[_id].userType = _userType;
+        transactions[_id].quantity = _quantity;
+        // transactions[_id].location_ = _location;
+        transactions[_id].transactionAddedBy = msg.sender;
         
         transactionsArr.push(_id) -1;
         
 
-        transactionInfo(_id, _drugId, _drugName, _buyerId, _buyerName, _sellerId, _sellerName, _quantity, _location, _address);
+        // transactionInfo(_id , _drugId, _drugName, _buyerId, _buyerName, _sellerId, _sellerName, _userType, _quantity, msg.sender);
+    
+        return _id;        
     }
     
     function getAllTransactions() view public returns(uint[]) {
+        
         return transactionsArr;
     }
     
-    function getTransactionDetails(uint _id) view public returns (uint, string, uint, string, address) {
-        return (transactions[_id].drugId, transactions[_id].drugName, transactions[_id].quantity, transactions[_id].location_, transactions[_id].transactionAddedBy);
+    function getTransactionDetails(uint _id) view public returns (uint, string, uint, uint, address) {
+        return (transactions[_id].drugId, transactions[_id].drugName, transactions[_id].quantity, transactions[_id].userType, transactions[_id].transactionAddedBy);
     }
     
-    function getBuyerSellerDetails(uint _id) view public returns (uint, string, uint, string, uint, string) {
-        return (transactions[_id].drugId, transactions[_id].drugName, transactions[_id].buyerId, transactions[_id].buyerName, transactions[_id].sellerId, transactions[_id].sellerName);
+    function getBuyerSellerDetails(uint _id) view public returns (uint, string, uint, string, uint, bytes32, bytes32) {
+        
+        return (transactions[_id].buyerId, transactions[_id].buyerName, transactions[_id].sellerId, transactions[_id].sellerName, blockNumber, blockHashNow, blockHashPrevious);
     }
     
     function countTransactions() view public returns (uint) {
